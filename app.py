0f02216837bf0373e3b59cfea88590784691c5ef
daple <114970828+DaffafauzanD@users.noreply.github.com>
2023-12-17 16:43:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,6 +101,6 @@ def invalid_route(e):
     return jsonify({'errorcode' : 404, 'message' : 'Route not Found'})
     
 if __name__ == '__main__':
-    app.run()
-
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
